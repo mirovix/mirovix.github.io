828ed8197e0bd09b0cc71bcbd45a83879c00d3c4
mirovix <miroljubmihailovic98@gmail.com>
2026-08-02 14:54:15 +0200

    fix: restore public homepage

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -4,12 +4,19 @@ const sh = require('shelljs');
 const renderPug = require('./render-pug');
 
 const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+const staticPath = upath.resolve(srcPath, 'static');
+const distPath = upath.resolve(upath.dirname(__filename), '../dist');
 
 sh.find(srcPath).forEach(_processFile);
+
+if (sh.test('-d', staticPath)) {
+    sh.cp('-R', `${staticPath}/*`, distPath);
+}
 
 function _processFile(filePath) {
     if (
         filePath.match(/\.pug$/)
+        && !filePath.match(/\/pug\/index\.pug$/)
         && !filePath.match(/include/)
         && !filePath.match(/mixin/)
         && !filePath.match(/\/pug\/layouts\//)
